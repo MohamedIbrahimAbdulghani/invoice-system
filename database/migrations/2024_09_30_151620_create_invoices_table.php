7ddd7c8aac7aee8e576575e3398c9176a9898e38
Mohamed Ibrahim <mohamedibrahimabdulghani@gmail.com>
2024-09-30 19:29:20 +0300

    added invoices page

---
+++ database/migrations/2024_09_30_151620_create_invoices_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('invoices', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('invoice_number');
+            $table->date('invoice_date');
+            $table->date('due_date'); // تاريخ الاستحقاق
+            $table->string('product');
+            $table->string('section');
+            $table->string('discount');
+            $table->string('rate_vat'); // نسبة الضريبة
+            $table->decimal('value_vat',8,2); // قيمة الضريبة
+            $table->decimal('Total',8,2);
+            $table->string('Status', 50);
+            $table->integer('value_status');
+            $table->text('note')->nullable();
+            $table->string('user');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('invoices');
+    }
+};
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\InvoicesController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,8 +20,8 @@ Route::get('/', function () {
     // return view('auth.login');
 });
 
+Route::resource('invoices', InvoicesController::class);
 Route::get('/{page}', [AdminController::class, "index"]);
-
 
 Route::middleware([
     'auth:sanctum',
