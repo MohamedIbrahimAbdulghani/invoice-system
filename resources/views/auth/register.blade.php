296ed8dd3b437a46b955e9bc7fd6122cfd112734
Mohamed Ibrahim <mohamedibrahimabdulghani@gmail.com>
2024-10-01 17:28:03 +0300

    edit about resource pages

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,62 +1,130 @@
-<x-guest-layout>
-    <x-authentication-card>
-        <x-slot name="logo">
-            <x-authentication-card-logo />
-        </x-slot>
+@extends('layouts.master2')
 
-        <x-validation-errors class="mb-4" />
+@section('title')
+إنشاء حساب
+@stop
 
-        <form method="POST" action="{{ route('register') }}">
-            @csrf
 
-            <div>
-                <x-label for="name" value="{{ __('Name') }}" />
-                <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            </div>
+@section('css')
+<!-- Sidemenu-respoansive-tabs css -->
+<link href="{{URL::asset('assets/plugins/sidemenu-responsive-tabs/css/sidemenu-responsive-tabs.css')}}"
+    rel="stylesheet">
+@endsection
+@section('content')
+<div class="container-fluid">
+    <div class="row no-gutter">
+        <!-- The image half -->
+        <!-- The content half -->
+        <div class="col-md-6 col-lg-6 col-xl-6 bg-white">
+            <div class="login d-flex align-items-center py-2">
+                <!-- Demo content-->
+                <div class="container p-0">
+                    <div class="row">
+                        <div class="col-md-10 col-lg-10 col-xl-9 mx-auto">
+                            <div class="card-sigin">
+                                <div class="mb-5 d-flex"> <a href="{{ url('/' . $page='Home') }}"><img
+                                            src="{{URL::asset('assets/img/brand/favicon.png')}}"
+                                            class="sign-favicon ht-40" alt="logo"></a>
+                                    <h1 class="main-logo1 ml-1 mr-0 my-auto tx-28">Invoice System</h1>
+                                </div>
+                                <div class="card-sigin">
+                                    <div class="main-signup-header">
+                                        <h2>مرحبا بك</h2>
+                                        <h5 class="font-weight-semibold mb-4">إنشاء حساب</h5>
+                                        <form method="POST" action="{{ route('register') }}">
+                                            @csrf
+                                            <div class="form-group">
+                                                <label>الاسم</label>
+                                                <input id="text" type="text"
+                                                    class="form-control @error('name') is-invalid @enderror" name="name"
+                                                    value="{{ old('name') }}" required autocomplete="name" autofocus>
+                                                @error('name')
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                                @enderror
+                                            </div>
 
-            <div class="mt-4">
-                <x-label for="email" value="{{ __('Email') }}" />
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-            </div>
+                                            <div class="form-group">
+                                                <label>البريد الالكتروني</label>
+                                                <input id="email" type="email"
+                                                    class="form-control @error('email') is-invalid @enderror"
+                                                    name="email" value="{{ old('email') }}" required
+                                                    autocomplete="email" autofocus>
 
-            <div class="mt-4">
-                <x-label for="password" value="{{ __('Password') }}" />
-                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
-            </div>
+                                                @error('email')
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                                @enderror
+                                            </div>
 
-            <div class="mt-4">
-                <x-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
-                <x-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
-            </div>
+                                            <div class="form-group">
+                                                <label>كلمة السر</label>
+                                                <input id="password" type="password"
+                                                    class="form-control @error('password') is-invalid @enderror"
+                                                    name="password" value="{{ old('password') }}" required
+                                                    autocomplete="password" autofocus>
 
-            @if (Laravel\Jetstream\Jetstream::hasTermsAndPrivacyPolicyFeature())
-                <div class="mt-4">
-                    <x-label for="terms">
-                        <div class="flex items-center">
-                            <x-checkbox name="terms" id="terms" required />
+                                                @error('password')
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                                @enderror
+                                            </div>
 
-                            <div class="ml-2">
-                                {!! __('I agree to the :terms_of_service and :privacy_policy', [
-                                        'terms_of_service' => '<a target="_blank" href="'.route('terms.show').'" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">'.__('Terms of Service').'</a>',
-                                        'privacy_policy' => '<a target="_blank" href="'.route('policy.show').'" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">'.__('Privacy Policy').'</a>',
-                                ]) !!}
+                                            <div class="form-group">
+                                                <label>تأكيد كلمة السر</label>
+                                                <input id="password_confirmation" type="password"
+                                                    class="form-control @error('password_confirmation') is-invalid @enderror"
+                                                    name="password_confirmation"
+                                                    value="{{ old('password_confirmation') }}" required
+                                                    autocomplete="password_confirmation" autofocus>
+
+                                                @error('password_confirmation')
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                                @enderror
+                                            </div>
+
+
+
+
+
+
+
+
+                                            <div class="flex items-center justify-end mt-4">
+                                                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 mb-3"
+                                                    href="{{ route('login') }}">
+                                                    {{ __('لدي اميل اريد تسجيل الدخول') }}
+                                                </a>
+                                                <button type="submit" class="btn btn-main-primary btn-block mt-3">
+                                                    {{ __('تسجيل ') }}
+                                                </button>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                    </x-label>
-                </div>
-            @endif
-
-            <div class="flex items-center justify-end mt-4">
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
-                    {{ __('Already registered?') }}
-                </a>
-
-                <x-button class="ml-4">
-                    {{ __('Register') }}
-                </x-button>
+                    </div>
+                </div><!-- End -->
             </div>
-        </form>
-    </x-authentication-card>
-</x-guest-layout>
+        </div><!-- End -->
 
+        <div class="col-md-6 col-lg-6 col-xl-6 d-none d-md-flex bg-primary-transparent">
+            <div class="row wd-100p mx-auto text-center">
+                <div class="col-md-12 col-lg-12 col-xl-12 my-auto mx-auto wd-100p">
+                    <img src="{{URL::asset('assets/img/1670311965993.jpg')}}"
+                        class="my-auto ht-xl-80p wd-md-100p wd-xl-80p mx-auto" alt="logo">
+                </div>
+            </div>
+        </div>
 
+    </div>
+</div>
+@endsection
+@section('js')
+@endsection
--- resources/views/invoices/invoices.blade.php
+++ resources/views/invoices/invoices.blade.php
@@ -7,80 +7,85 @@
 <link href="{{URL::asset('assets/plugins/datatable/css/jquery.dataTables.min.css')}}" rel="stylesheet">
 <link href="{{URL::asset('assets/plugins/datatable/css/responsive.dataTables.min.css')}}" rel="stylesheet">
 @endsection
+
+@section('title')
+الفواتير
+@stop
 @section('page-header')
-				<!-- breadcrumb -->
-				<div class="breadcrumb-header justify-content-between">
-					<div class="my-auto">
-						<div class="d-flex">
-							<h4 class="content-title mb-0 my-auto">قائمة الفواتير</h4><span class="text-muted mt-1 tx-13 mr-2 mb-0">/ الفواتير</span>
-						</div>
-					</div>
-				</div>
-				<!-- breadcrumb -->
+<!-- breadcrumb -->
+<div class="breadcrumb-header justify-content-between">
+    <div class="my-auto">
+        <div class="d-flex">
+            <h4 class="content-title mb-0 my-auto">قائمة الفواتير</h4><span class="text-muted mt-1 tx-13 mr-2 mb-0">/
+                الفواتير</span>
+        </div>
+    </div>
+</div>
+<!-- breadcrumb -->
 @endsection
 @section('content')
-				<!-- row -->
-				<div class="row row-sm">
-					
-					<!--div-->
-					<div class="col-xl-12">
-						<div class="card mg-b-20">
-							<div class="card-header pb-0">
-								<div class="d-flex justify-content-between">
-									<h4 class="card-title mg-b-0">Bordered Table</h4>
-									<i class="mdi mdi-dots-horizontal text-gray"></i>
-								</div>
-								<p class="tx-12 tx-gray-500 mb-2">Example of Valex Bordered Table.. <a href="">Learn more</a></p>
-							</div>
-							<div class="card-body">
-								<div class="table-responsive">
-									<table id="example" class="table key-buttons text-md-nowrap">
-										<thead>
-											<tr>
-												<th class="border-bottom-0">#</th>
-												<th class="border-bottom-0">رقم الفاتورة</th>
-												<th class="border-bottom-0">تاريخ الفاتورة</th>
-												<th class="border-bottom-0">تاريخ الاستحقاق</th>
-												<th class="border-bottom-0">المنتج</th>
-												<th class="border-bottom-0">القسم</th>
-												<th class="border-bottom-0">الخصم</th>
-												<th class="border-bottom-0">نسبة الضريبة</th>
-												<th class="border-bottom-0">قيمة الضريبة</th>
-												<th class="border-bottom-0">الاجمالي</th>
-												<th class="border-bottom-0">الحالة</th>
-												<th class="border-bottom-0">الملاحظات</th>
-											</tr>
-										</thead>
-										<tbody>
-											
-											<tr>
-												<td>Herrod Chandler</td>
-												<td>Sales Assistant</td>
-												<td>San Francisco</td>
-												<td>59</td>
-												<td>2012/08/06</td>
-												<td>$137,500</td>
-                                                <td>Herrod Chandler</td>
-												<td>Sales Assistant</td>
-												<td>San Francisco</td>
-												<td>59</td>
-												<td>2012/08/06</td>
-												<td>$137,500</td>
-											</tr>
-										</tbody>
-									</table>
-								</div>
-							</div>
-						</div>
-					</div>
-					<!--/div-->
+<!-- row -->
+<div class="row row-sm">
 
-				</div>
-				<!-- row closed -->
-			</div>
-			<!-- Container closed -->
-		</div>
-		<!-- main-content closed -->
+    <!--div-->
+    <div class="col-xl-12">
+        <div class="card mg-b-20">
+            <div class="card-header pb-0">
+                <div class="d-flex justify-content-between">
+                    <h4 class="card-title mg-b-0">Bordered Table</h4>
+                    <i class="mdi mdi-dots-horizontal text-gray"></i>
+                </div>
+                <p class="tx-12 tx-gray-500 mb-2">Example of Valex Bordered Table.. <a href="">Learn more</a></p>
+            </div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table id="example" class="table key-buttons text-md-nowrap">
+                        <thead>
+                            <tr>
+                                <th class="border-bottom-0">#</th>
+                                <th class="border-bottom-0">رقم الفاتورة</th>
+                                <th class="border-bottom-0">تاريخ الفاتورة</th>
+                                <th class="border-bottom-0">تاريخ الاستحقاق</th>
+                                <th class="border-bottom-0">المنتج</th>
+                                <th class="border-bottom-0">القسم</th>
+                                <th class="border-bottom-0">الخصم</th>
+                                <th class="border-bottom-0">نسبة الضريبة</th>
+                                <th class="border-bottom-0">قيمة الضريبة</th>
+                                <th class="border-bottom-0">الاجمالي</th>
+                                <th class="border-bottom-0">الحالة</th>
+                                <th class="border-bottom-0">الملاحظات</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+
+                            <tr>
+                                <td>Herrod Chandler</td>
+                                <td>Sales Assistant</td>
+                                <td>San Francisco</td>
+                                <td>59</td>
+                                <td>2012/08/06</td>
+                                <td>$137,500</td>
+                                <td>Herrod Chandler</td>
+                                <td>Sales Assistant</td>
+                                <td>San Francisco</td>
+                                <td>59</td>
+                                <td>2012/08/06</td>
+                                <td>$137,500</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--/div-->
+
+</div>
+<!-- row closed -->
+</div>
+<!-- Container closed -->
+</div>
+<!-- main-content closed -->
 @endsection
 @section('js')
 <!-- Internal Data tables -->
