9c77709600ef11778b6cdec7651016320e00ebe2
mohamedibrahimabdulghani <mohamedibrahimabdulghani@gmail.com>
2025-01-17 16:56:51 +0200

    make validation about delete invoice_attachemnt

--- resources/views/invoices/invoices_details.blade.php
+++ resources/views/invoices/invoices_details.blade.php
@@ -25,6 +25,16 @@
 @endsection
 @section('content')
 
+
+@if (session()->has('Delete'))
+<div class="alert alert-danger alert-dismissible fade show" role="alert">
+    <strong>{{ session()->get('Delete') }}</strong>
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+    </button>
+</div>
+@endif
+
 <div class="col-xl-12">
     <!-- div -->
     <div class="card" id="tabs-style4">
